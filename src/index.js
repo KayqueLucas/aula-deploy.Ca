74e1a25f75f24be93f7010ce8ec0e61102346b62
Kayquedev <kayquedesigner@gmail.com>
2023-10-24 01:25:54 -0300

    Commit inicial

---
+++ src/index.js
@@ -0,0 +1,14 @@
+require('dotenv').config()
+const express = require('express')
+const { async } = require('rxjs')
+const app = express()
+app.use(express.json())
+
+const port = process.env.PORT || 3000
+app.get('/', async (req, res) => {
+    return res.json('Api esta Ok!')
+})
+
+app.listen(port, () => {
+    console.log(`Servidor Ligado na porta ${port}`);
+})
